a69496ef7bcf052d5504e2465bb6f6a51a838475
dshank <shank.david@gmail.com>
2024-07-06 11:42:50 -0400

    Add better terminal keybinds
    Add keybinding for <Esc> to exit terminal insert mode
    
    Adds keybindings for <Alt>+[hjkl] to move between buffers, including
    terminal buffers
    
    Changes the <Ctrl>+[hjkl] bindings to <Alt>+[hjkl]

--- lua/config.lua
+++ lua/config.lua
@@ -12,15 +12,23 @@ vim.opt.autoindent = true
 vim.opt.number = true
 vim.opt.ttyfast = true
 
--- Quicker split switching
-vim.keymap.set("n", "<C-K>", "<C-W>k", { noremap = true, silent = true, nowait = true })
-vim.keymap.set("n", "<C-J>", "<C-W>j", { noremap = true, silent = true, nowait = true })
-vim.keymap.set("n", "<C-H>", "<C-W>h", { noremap = true, silent = true, nowait = true })
-vim.keymap.set("n", "<C-L>", "<C-W>l", { noremap = true, silent = true, nowait = true })
-
 -- Set LSP log level to reduce disk usage and prevent performance degradation
 vim.lsp.set_log_level("WARN")
+
+-- Quicker split switching using <Alt>
+vim.keymap.set({"n", "v"}, "<A-k>", "<C-w>k", { noremap = true, silent = true, nowait = true })
+vim.keymap.set({"n", "v"}, "<A-j>", "<C-w>j", { noremap = true, silent = true, nowait = true })
+vim.keymap.set({"n", "v"}, "<A-h>", "<C-w>h", { noremap = true, silent = true, nowait = true })
+vim.keymap.set({"n", "v"}, "<A-l>", "<C-w>l", { noremap = true, silent = true, nowait = true })
+-- And in terminal mode
+vim.keymap.set({"t"}, "<A-k>", "<C-\\><C-n><C-w>k", { noremap = true, silent = true, nowait = true })
+vim.keymap.set({"t"}, "<A-j>", "<C-\\><C-n><C-w>j", { noremap = true, silent = true, nowait = true })
+vim.keymap.set({"t"}, "<A-h>", "<C-\\><C-n><C-w>h", { noremap = true, silent = true, nowait = true })
+vim.keymap.set({"t"}, "<A-l>", "<C-\\><C-n><C-w>l", { noremap = true, silent = true, nowait = true })
 
 -- Moving up and down doesn't line wrap for lines longer than the screen
 vim.keymap.set({"n", "v"}, "j", "gj", { noremap = true, silent = true, nowait = true })
 vim.keymap.set({"n", "v"}, "k", "gk", { noremap = true, silent = true, nowait = true })
+
+-- Escape terminal via <ESC>
+vim.keymap.set({"t"}, "<Esc>", "<C-\\><C-N>", { noremap = true, silent = true, nowait = true })
