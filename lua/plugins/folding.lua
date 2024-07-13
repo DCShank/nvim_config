a7ea190709e07c98dfc6e8d7187b49442a42e3b0
dshank <shank.david@gmail.com>
2024-07-13 13:50:18 -0400

    Improve fold line to show number of lines folded

--- lua/plugins/folding.lua
+++ lua/plugins/folding.lua
@@ -16,6 +16,33 @@ Folding.keys = {
     { '<space><space>', 'za',                         mode = 'n' },
 }
 
+local fold_text_handler = function(virtText, lnum, endLnum, width, truncate)
+    local newVirtText = {}
+    local suffix = (' 󰁂 %d '):format(endLnum - lnum)
+    local sufWidth = vim.fn.strdisplaywidth(suffix)
+    local targetWidth = width - sufWidth
+    local curWidth = 0
+    for _, chunk in ipairs(virtText) do
+        local chunkText = chunk[1]
+        local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+        if targetWidth > curWidth + chunkWidth then
+            table.insert(newVirtText, chunk)
+        else
+            chunkText = truncate(chunkText, targetWidth - curWidth)
+            local hlGroup = chunk[2]
+            table.insert(newVirtText, {chunkText, hlGroup})
+            chunkWidth = vim.fn.strdisplaywidth(chunkText)
+            -- str width returned from truncate() may less than 2nd argument, need padding
+            if curWidth + chunkWidth < targetWidth then
+                suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+            end
+            break
+        end
+        curWidth = curWidth + chunkWidth
+    end
+    table.insert(newVirtText, {suffix, 'MoreMsg'})
+    return newVirtText
+end
 
 function Folding.init()
     vim.o.foldcolumn = '0' -- '0' is not bad
@@ -29,42 +56,7 @@ Folding.opts = {
     close_fold_kinds_for_ft = {
         default = { 'imports', 'comment' },
     },
+    fold_virt_text_handler = fold_text_handler,
 }
-
---function Folding.config()
---    vim.o.foldcolumn = '0' -- '0' is not bad
---    vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
---    vim.o.foldlevelstart = 99
---    vim.o.foldenable = true
---    -- Using ufo provider need remap `zR` and `zM`
---    vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
---    vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
---
---    vim.keymap.set({ 'n', 'v' }, "<space>", "<Nop>")
---    vim.keymap.set({ 'n', 'v' }, "<space><space>", "za")
---    -- I set this up in the language-server config file, as it conflicts with the capabilities I setup there otherwise
---    -- Option 2: nvim lsp as LSP client
---    -- Tell the server the capability of foldingRange,
---    -- Neovim hasn't added foldingRange to default capabilities, users must add it manually
---    --local capabilities = vim.lsp.protocol.make_client_capabilities()
---    --local capabilities = require('cmp_nvim_lsp').default_capabilities()
---    --capabilities.textDocument.foldingRange = {
---    --    dynamicRegistration = false,
---    --    lineFoldingOnly = true
---    --}
---    --local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
---    --for _, ls in ipairs(language_servers) do
---    --    require('lspconfig')[ls].setup({
---    --        capabilities = capabilities
---    --        -- you can add other fields for setting up lsp server in this table
---    --    })
---    --end
---    require('ufo').setup({
---        open_fold_hl_timeout = 150,
---        close_fold_kinds_for_ft = {
---            default = { 'imports', 'comment' },
---        },
---    })
---end
 
 return Folding
