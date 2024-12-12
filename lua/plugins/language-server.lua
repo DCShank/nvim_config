0c9991d1c904758baf57818c3041b86cc68b05eb
dshank <shank.david@gmail.com>
2024-12-11 21:41:45 -0500

    Allow formatting in visual selection range

--- lua/plugins/language-server.lua
+++ lua/plugins/language-server.lua
@@ -63,7 +63,7 @@ function LSPConfig.config()
 
             vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, opts)
             vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-            vim.keymap.set('n', '<leader>f', function()
+            vim.keymap.set({ 'n', 'v' }, '<leader>f', function()
                 vim.lsp.buf.format { async = true }
             end, opts)
         end,
